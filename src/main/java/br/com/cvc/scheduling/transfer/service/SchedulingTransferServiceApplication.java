973ce09ca46d05f02429b6e11a430231d6a561c1
x229466 <victor.r.matos@accenture.com>
2020-06-18 20:23:50 -0300

    Initial commit :tada:

---
+++ src/main/java/br/com/cvc/scheduling/transfer/service/SchedulingTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc.scheduling.transfer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cvc/scheduling/transfer/service/SchedulingTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cvc.scheduling.transfer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
